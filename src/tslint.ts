6dc45b2ec8092258402dfa0b1c828790136cfe7b
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-06-19 23:57:53 +0200

    feat(all): initial commit

---
+++ src/tslint.ts
@@ -0,0 +1,19 @@
+import {WebpackConfig, get} from '@easy-webpack/core'
+import * as path from 'path'
+
+/**
+ * Tslint loader support for *.ts files
+ * See: https://github.com/wbuchwalter/tslint-loader
+ */
+export function tslint(options: any = {}, exclude: Array<string> = null) {
+  return function tslint(this: WebpackConfig): WebpackConfig {
+    return {
+      module: {
+        preLoaders: get(this, 'module.preLoaders', []).concat([
+          { test: /\.tsx?$/, loader: 'tslint', exclude: exclude || this.metadata.root ? [path.join(this.metadata.root, 'node_modules')] : [] }
+        ])
+      },
+      tslint: options
+    }
+  }
+}
