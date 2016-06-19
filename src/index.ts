f293139aa92b8c1caf91136dc51b1dacc565a42b
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-06-19 23:58:29 +0200

    fix(index): correct main file

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,18 @@ import {WebpackConfig, get} from '@easy-webpack/core'
 import * as path from 'path'
 
 /**
- * Babel loader support for ES2015
- * See: https://github.com/babel/babel-loader
+ * Tslint loader support for *.ts files
+ * See: https://github.com/wbuchwalter/tslint-loader
  */
-export function babel(options = {
-    plugins: ['transform-decorators-legacy'],
-    presets: ['es2015-loose-native-modules', 'stage-1'],
-    cacheDirectory: true,
-  }, exclude: Array<string> = null) {
-  return function babel(this: WebpackConfig): WebpackConfig {
+export function tslint(options: any = {}, exclude: Array<string> = null) {
+  return function tslint(this: WebpackConfig): WebpackConfig {
     return {
       module: {
-        loaders: get(this, 'module.loaders', []).concat([{
-          test: /\.jsx?$/,
-          loader: 'babel',
-          exclude: exclude || this.metadata.root ? [path.join(this.metadata.root, 'node_modules')] : [],
-          query: options
-        }])
-      }
+        preLoaders: get(this, 'module.preLoaders', []).concat([
+          { test: /\.tsx?$/, loader: 'tslint', exclude: exclude || this.metadata.root ? [path.join(this.metadata.root, 'node_modules')] : [] }
+        ])
+      },
+      tslint: options
     }
   }
 }
