b98475c2815b56c7fc07cb57a458cb0f4e9ddbf6
David Weber <david.weber@w3tec.ch>
2016-11-13 23:21:07 +0100

    remove options as it has to be provided over options plugin from webpack >=2.1.0-beta.23

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,18 @@
 import {WebpackConfigWithMetadata, get} from '@easy-webpack/core'
 import * as path from 'path'
-import * as webpack from 'webpack'
 
 /**
  * Tslint loader support for *.ts files
  * See: https://github.com/wbuchwalter/tslint-loader
  */
-export = function tslint({options = undefined, exclude = null} = {}) {
+export = function tslint({exclude = null} = {}) {
   return function tslint(this: WebpackConfigWithMetadata): WebpackConfigWithMetadata {
     return {
       module: {
         rules: get(this, 'module.rules', []).concat([
           { test: /\.tsx?$/, loader: 'tslint-loader', enforce: 'pre', exclude: exclude || (this.metadata.root ? [path.join(this.metadata.root, 'node_modules')] : []) }
         ])
-      },
-      plugins: [
-        new webpack.LoaderOptionsPlugin({
-          options: {
-            tslint: options
-          }
-        })
-      ]
+      }
     }
   }
 }
